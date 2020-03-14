968d56b76371a82c01bed55a872eee9d958b8041
RAILER5500 <56223647+gotchik648@users.noreply.github.com>
2020-03-14 12:34:33 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// let str = "Привіт";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(flag));
+// console.log(typeof(txt));
+
+// let str = "Вітя - как дела",
+//     flag = false,
+//     obj = {},
+//     number = 1368,
+//     array2 = [1,2,3];
+// const num = 1441,
+//     txt = "false",
+//     point = true;
+//     array1 = ["hello"],
+//     string = "buy cloths!",
+
+// let myAge = 13,
+//     kolyaAge = 13,
+//     sum = myAge+arturAge;
+// alert(sum);
+// let n1 = 108,
+//     n2 = 223,
+//     n3 = 2,
+//     n4 = 5,
+//     result = n1+n2-n3*n4;
+// console.log(result);
+
+// let a1 = 5+3,
+//     a2 = 5-3,
+//     a3 = 5*3,
+//     a4 = 5/3,
+//     a5 = 5**3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
