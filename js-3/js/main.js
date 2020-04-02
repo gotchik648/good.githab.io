86ee85976eab6fd35134a4a076f6544ec421fff7
RAILER5500 <56223647+gotchik648@users.noreply.github.com>
2020-04-02 19:45:00 +0300

    Add files via upload

---
+++ js-3/js/main.js
@@ -0,0 +1,33 @@
+// const data = 1;
+// if (data==1) {
+//     console.log("Сьогодні перше число!");
+// } else {
+//     console.log("Сьогодні: ", data);
+// }
+
+
+// const data = 1;
+// if (data%2==0) {
+//     console.log(data);
+// } else {
+//     console.log("upppss");
+// }
+
+
+// let a = 5%2;
+
+// if (a==0) {
+//     console.log(5%2 + " грн");
+// } else {
+//     console.log(a + " євро");
+// }
+
+
+// let maks = 55;
+// if (maks>0 && maks<17) {
+//     console.log('kids');
+// } else if (maks>16 && maks<61) {
+//     console.log("adult");
+// } else {
+//     console.log("old");
+// }
