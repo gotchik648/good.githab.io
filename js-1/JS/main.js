69f2c7d9f7519cdd766c40839de4e4d68aa4bf6f
RAILER5500 <56223647+gotchik648@users.noreply.github.com>
2020-04-26 15:37:09 +0300

    Add files via upload

---
+++ js-1/JS/main.js
@@ -0,0 +1,45 @@
+
+let str = "Привіт";
+let num = 123;
+let flag = true;
+let txt = "true";
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(flag));
+console.log(typeof(txt));
+
+
+let str = "приветик",
+    num = 123,
+    flag = false,
+    txt = "как делишки",
+    obj = {};
+const array1 = [даров],
+      array2 = ["123"],
+      string = "майнкрафт топ",
+      number = 1234124,
+      boolean = "ролики";
+
+
+let myAge = 13,
+    arturAge = 13,
+    sum = myAge+arturAge;
+alert(sum);
+ let n1 = 108,
+    n2 = 223,
+    n3 = 2,
+    n4 = 5,
+    result = n1+n2-n3*n4;
+console.log(result);
+
+
+let a1 = 5+3,
+    a2 = 5-3,
+    a3 = 5*3,
+    a4 = 5/3,
+    a5 = 5**3;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
