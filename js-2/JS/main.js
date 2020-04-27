5a9a460577222e8763c522d5e3f14977fb4655a9
RAILER5500 <56223647+gotchik648@users.noreply.github.com>
2020-04-27 19:56:32 +0300

    Add files via upload

---
+++ js-2/JS/main.js
@@ -0,0 +1,38 @@
+// number1
+// let k1 = '5px',
+// k2 = '12seghurhguierj334',
+// k3 = '12 feyfghuieghui.14',
+// k4 = 'qwertyuiop';
+// console.log (Number.parseInt (k1));
+// console.log (Number.parseInt (k2));
+// console.log (Number.parseInt (k3));
+// console.log (Number.parseInt (k4));
+
+
+// number2
+// let k1 = '5px',
+// k2 = '12seghurhguierj334',
+// k3 = '12 feyfghuieghui.14',
+// k4 = 'qwertyuiop';
+// console.log (Number.parseFloat (k1));
+// console.log (Number.parseFloat (k2));
+// console.log (Number.parseFloat (k3));
+// console.log (Number.parseFloat (k4));
+
+
+// number3
+// let random = Math.floor (Math.random () * (10-1) +1);
+// console.log (Math.pow (random, 2));
+
+
+// number4
+// const a = 'My',
+//       b = 'name',
+//       c = 'is';
+// fullName = '$ {a} $ {b} $ {c} Vitya!';
+// console.log (fullName);
+
+
+// number5
+// const result = 5 + 5 + '5';
+// console.log (result);
